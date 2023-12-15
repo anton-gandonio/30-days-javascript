547a26afbee56dc18d120a3fd49a29c1063d57ae
anton-gandonio <for.site.joins@gmail.com>
2023-12-15 23:37:39 +0500

    first commit

---
+++ js-forms/script.js
@@ -0,0 +1,12 @@
+const docs = document.forms
+const file = document.forms.file
+const textForm = document.forms.textForm
+const rightFirst = textForm.rightFirst
+const rightSec = document.forms.rightSec
+const leftFirst = document.forms.leftFirst
+const leftSec = document.forms.leftSec
+const leftThird = document.forms.leftThird
+console.log(document.forms);
+
+console.log(docs.elements);
+console.log(rightFirst.value);
